d8d05b078188b013d011b0d23edaa6b8605177b9
stevenorum <stevenorum@gmail.com>
2017-06-20 21:05:56 -0400

    Update mac.ino

--- mac.ino
+++ mac.ino
@@ -1,52 +1,53 @@
 #include <Keyboard.h>
 
-void pressAndRelease(char c) {
-  Keyboard.press(c);
-  Keyboard.release(c);
+int DELAY_MULTIPLIER = 1; // for testing
+void _delay(int i) { delay(DELAY_MULTIPLIER * i); }
+
+int OS = -1, MAC = 0, WIN = 1, LNX = 2;
+char CMD_KEYS[3] = {char(KEY_LEFT_GUI), char(KEY_LEFT_CTRL), char(KEY_LEFT_CTRL)};
+
+void press_and_release(String s) {
+  for (int i = 0; i < s.length(); i++) {Keyboard.press(s.charAt(i));}
+  for (int i = 0; i < s.length(); i++) {Keyboard.release(s.charAt(i));}
+}
+void press_and_release(int i) { press_and_release(String(char(i))); }
+void tab() { press_and_release(KEY_TAB); }
+void enter() { press_and_release(KEY_RETURN); }
+void cmd(char c) { press_and_release(String(CMD_KEYS[OS]) + c); }
+void type(String s) {
+  for (int i=0; i < s.length(); i++){
+    press_and_release(s.charAt(i));
+  } 
 }
 
-void tab() { pressAndRelease(KEY_TAB); }
+void send_email(String recipient, String subject, String contents) {
+  if (MAC == OS) {
+    // Open email client (outlook, in this case)
+    cmd(' '); _delay(100); // open spotlight
+    type("outloo"); enter(); _delay(250); // search for outlook
 
-void enter() { pressAndRelease(KEY_RETURN); }
-
-void cmd(char c) {
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press(c);
-  Keyboard.releaseAll();
-}
-
-void type(String s) { for (int i=0; i < s.length(); i++){ pressAndRelease(s.charAt(i)); } }
-
-void open_outlook() {
-  cmd(' ');
-  delay(100);
-  type("outloo");
-  enter();
-  delay(250);
-}
-
-void email(String recipient, String subject, String contents) {
-  cmd('n');
-  delay(400);
-  type(recipient);
-  delay(300);
-  tab();
-  enter();
-  delay(300);
-  type(subject);
-  delay(300);
-  tab();
-  delay(400);
-  type(contents);
-  enter();
-  cmd(KEY_RETURN);
+    // Send the message
+    cmd('n'); _delay(400); // open a new message
+    type(recipient); _delay(300); // enter the recipient string
+    tab(); enter(); _delay(300); // tab down to the subject line
+    type(subject); _delay(300); // enter the subject
+    tab(); _delay(400); // tab down to the message body    
+    type(contents); enter(); // enter the message contents
+    //cmd(KEY_RETURN); // send - Commented out to make it safe to test; uncomment this command to weaponize this.
+  }
 }
 
 void setup() {
+  // Eventual plan is to have the OS be determined based on which button of the USB hub is depressed.
+  // The board I'm using should just barely be able to fit inside a Sabrent 4-port USB hub without removing any guts.
+  // One of the ports will be swapped and used for the board.
+  // The other three will be active but the LED power lines will be tapped to use as signals to the arduino.
+  // One button for Mac, one button for Windows, one button for Linux, and one on/off button for the arduino itself.
+  // By running at startup, it's easy to re-send the message by powering it down and then back up.
+  OS = MAC;
   Keyboard.begin();
-  delay(500);
-  open_outlook();
-  email(
+  _delay(500);
+  send_email(
     "your_department@HipstrStartup.co",
     "I love donuts!!1!",
     "Dear everybody,\n\nI love donuts, so I'll bring some to share tomorrow.\n\n-Someone who forgot to lock their computer"
